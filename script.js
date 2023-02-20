8dd7f1e212e4bd54c4fc59602085d1b51dca033c
MaksymDramaretskyi <maksymdramaretskyi@gmail.com>
2023-02-20 23:20:36 +0200

    word my portfolio

---
+++ script.js
@@ -0,0 +1,12 @@
+var myHeading = document.querySelector('.header__link');
+myHeading.textContent = 'Hello world!';
+
+// alert('Hello')
+
+// let userName = prompt('What is your name?');
+// let userOld = prompt('How old are you?');
+// let userFrom = prompt('Where are you from');
+
+// let userAll = 'Hello!' + ' ' + userName + '! ' + 'You are' + ' ' +  userOld + ' ' + 'years old!' + ' '  + 'You live in' + ' ' + userFrom ;
+// alert(userAll)
+
