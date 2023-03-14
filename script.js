affd76028d0916c0d5c4a3470ce17ee7aaa69bcd
MaksymDramaretskyi <maksymdramaretskyi@gmail.com>
2023-03-14 21:58:20 +0200

    add block work and skills

--- script.js
+++ script.js
@@ -88,7 +88,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("burger").addEventListener("click", function(){
     document.querySelector("header").classList.toggle("open");
-    document.querySelector("body").classList.toggle("lock")
+    document.querySelector("body").classList.toggle("lock");
   })
 })
 
