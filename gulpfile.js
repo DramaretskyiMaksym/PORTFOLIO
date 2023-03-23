58bf026f136d52c5f4e2f7802c281924a35d8b59
MaksymDramaretskyi <maksymdramaretskyi@gmail.com>
2023-03-23 19:05:25 +0200

    add animation and change color site

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const sass = require('gulp-sass')(require('sass'));
 gulp.task ('css', ()=> {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./css')) 
 })
 
 gulp.watch("./scss/**/*.scss", gulp.series("css"));
